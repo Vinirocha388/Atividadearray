6e4ef3f2695d751468f6ef9991231e32a04de15b
Vinícius Rocha <158187426+Vinirocha388@users.noreply.github.com>
2024-03-16 20:07:31 -0300

    Update atividadearray.js

--- atividadearray.js
+++ atividadearray.js
@@ -1,7 +1,1 @@
-//Métodos Array 
 
-//Push:Adiciona um ou mais elementos ao final de um array e retorna o novo comprimento do array.
-//Exemplo 1
-let numbers = [1, 2, 3]; // Cria um array com elementos;
-numbers.push(4); // Adiciona o elemento 4 ao final do array;
-console.log(numbers); // Saída: [1, 2, 3, 4] - Exibe o array atualizado;
