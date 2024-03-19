c5046a425d6364c0553f9e96c1a4e4311d02fadc
unknown <virocha.tds2.senai@gmail.com>
2024-03-19 17:16:39 -0300

    Terminar me casa

--- contagemvogais.js
+++ contagemvogais.js
@@ -1,8 +1,9 @@
 //Contagem de vogais
-let palavra = "Vinicius";
+let palavra = "Vinicius Rocha";
 let palavra1 = palavra.split("");
 let vogais = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
 let quanvogais = palavra1.filter(letras => vogais.includes(letras));
-console.log("A quantidade de vogais é: " + quanvogais.length);
 console.log(quanvogais);
+console.log("A quantidade de vogais é: " + quanvogais.length);
+
 
